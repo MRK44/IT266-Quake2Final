2a513287903d3ac85847ab53e84dd6e80bd0b5ee
Mohamed <Khanantisora@gmail.com>
2018-12-17 20:39:27 -0500

    The Famas has been created. Less ammo count but more damage.

--- p_client.c
+++ p_client.c
@@ -604,7 +604,7 @@ void InitClientPersistant (gclient_t *client)
 	client->pers.health			= 100;
 	client->pers.max_health		= 100;
 	// GOING TO CHANGE THE BULLET CAPAICITY LATER FOR FAMAS. - GAME MOD
-	client->pers.max_bullets	= 200;
+	client->pers.max_bullets	= 25;
 	client->pers.max_shells		= 100;
 	client->pers.max_rockets	= 50;
 	client->pers.max_grenades	= 50;
